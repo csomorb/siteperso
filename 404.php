ab40f6a07b481fbbec2ef3bfe8b2eb73baeed466
Barna <csomorbarnabas@yahoo.com>
2016-10-13 00:23:44 +0200

    404

--- 404.php
+++ 404.php
@@ -6,5 +6,5 @@
  * Time: 23:51
  */
 ?>
-
-<h1>Page introuvable</h1>
+<img class="responsive-img" src="./img/h315.JPG">
+<h1 class="center h404">404 - Page introuvable</h1>
--- index.php
+++ index.php
@@ -21,6 +21,7 @@ $page = "pages/" . $params . ".php";
     <meta charset="UTF-8">
     <title>Barnabas Csomor</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
